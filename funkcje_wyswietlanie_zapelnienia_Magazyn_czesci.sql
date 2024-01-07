0b8c6757e1f2c2fe8d036332358238bb4be1c059
PatrykW7 <patryk90020@o2.pl>
2024-01-07 22:43:42 +0100

    Powoli do przodu

--- funkcje_wyswietlanie_zapelnienia_Magazyn_czesci.sql
+++ funkcje_wyswietlanie_zapelnienia_Magazyn_czesci.sql
@@ -34,3 +34,63 @@ BEGIN
     CLOSE v_result_cursor;
 END;
 /
+
+
+
+----------------- OBIEKT + KOLEKCJE 
+
+
+CREATE OR REPLACE TYPE PartStorageInfo AS OBJECT (
+    Lokacja_ID INT,
+    Czesc_ID INT,
+    Ilosc_w_magazynie INT,
+    Max_pojemnosc INT,
+    Czesc_zapelnienie NUMBER
+);
+/
+
+CREATE OR REPLACE TYPE PartStorageInfoList AS TABLE OF PartStorageInfo;
+/
+
+CREATE OR REPLACE FUNCTION CheckPartStorageFullness
+RETURN PartStorageInfoList
+AS
+    v_part_storage_list PartStorageInfoList := PartStorageInfoList();
+    v_index INT := 1;
+BEGIN
+    FOR storage_info IN (
+        SELECT LOKACJA_ID, CZESC_ID, ILOSC_W_MAGAZYNIE, MAX_POJEMNOSC,
+               ILOSC_W_MAGAZYNIE / MAX_POJEMNOSC * 100 AS PartStorageFullnes
+        FROM Magazyn_czesci
+    )
+    LOOP
+        v_part_storage_list.extend;
+        v_part_storage_list(v_index) := PartStorageInfo(
+            storage_info.LOKACJA_ID,
+            storage_info.CZESC_ID,
+            storage_info.ILOSC_W_MAGAZYNIE,
+            storage_info.MAX_POJEMNOSC,
+            storage_info.PartStorageFullnes
+        );
+        v_index := v_index + 1;
+    END LOOP;
+
+    RETURN v_part_storage_list;
+END;
+/
+
+--------------- WYWOLANIE 
+
+DECLARE
+    v_result PartStorageInfoList := PartStorageInfoList();
+BEGIN
+    v_result := CheckPartStorageFullness;
+
+    FOR i IN 1..v_result.count LOOP
+        DBMS_OUTPUT.PUT_LINE(
+            'Lokacja: ' || v_result(i).Lokacja_ID || ', część: ' || v_result(i).Czesc_ID ||
+            ', zapełnienie: ' || ROUND(v_result(i).Czesc_zapelnienie, 2) || '%'
+        );
+    END LOOP;
+END;
+/
--- funkcje_wyswietlanie_zapelnienia_Magazyn_produktow.sql
+++ funkcje_wyswietlanie_zapelnienia_Magazyn_produktow.sql
@@ -36,3 +36,79 @@ BEGIN
     CLOSE v_result_cursor;
 END;
 /
+
+
+--- KOLEKCJA + OBIEKT
+
+CREATE OR REPLACE TYPE ProductStorageInfo AS OBJECT (
+    Lokacja_ID INT,
+    Lokacja_nazwa VARCHAR(45),
+    Ilosc_w_magazynie INT,
+    Maksymalna_pojemnosc INT,
+    Produkt_ID INT,
+    Produkt_zapelnienie NUMBER
+);
+/
+
+CREATE OR REPLACE TYPE ProductStorageInfoList AS TABLE OF ProductStorageInfo;
+/
+
+CREATE OR REPLACE FUNCTION CheckProductStorageFullness
+RETURN ProductStorageInfoList
+AS
+    v_product_storage_list ProductStorageInfoList := ProductStorageInfoList();
+    v_index INT := 1;
+BEGIN
+    FOR storage_info IN (
+        SELECT LOKACJA_ID, LOKACJA_NAZWA, ILOSC_W_MAGAZYNIE, MAKSYMALNA_POJEMNOSC, PRODUKT_ID,
+               ILOSC_W_MAGAZYNIE / MAKSYMALNA_POJEMNOSC * 100 AS ProductStorageFullnes
+        FROM Magazyn_produktow
+    )
+    LOOP
+        v_product_storage_list.extend;
+        v_product_storage_list(v_index) := ProductStorageInfo(
+            storage_info.LOKACJA_ID,
+            storage_info.LOKACJA_NAZWA,
+            storage_info.ILOSC_W_MAGAZYNIE,
+            storage_info.MAKSYMALNA_POJEMNOSC,
+            storage_info.PRODUKT_ID,
+            storage_info.ProductStorageFullnes
+        );
+        v_index := v_index + 1;
+    END LOOP;
+
+    RETURN v_product_storage_list;
+END;
+/
+
+DECLARE
+    v_result ProductStorageInfoList := ProductStorageInfoList();
+BEGIN
+    v_result := CheckProductStorageFullness;
+
+    FOR i IN 1..v_result.count LOOP
+        DBMS_OUTPUT.PUT_LINE(
+            'Lokacja: ' || v_result(i).Lokacja_ID || ', produkt: ' || v_result(i).Produkt_ID ||
+            ', zapełnienie: ' || ROUND(v_result(i).Produkt_zapelnienie, 2) || '%'
+        );
+    END LOOP;
+END;
+/
+
+
+
+-- WYWOLANIE 
+
+DECLARE
+    v_result ProductStorageInfoList := ProductStorageInfoList();
+BEGIN
+    v_result := CheckProductStorageFullness;
+
+    FOR i IN 1..v_result.count LOOP
+        DBMS_OUTPUT.PUT_LINE(
+            'Lokacja: ' || v_result(i).Lokacja_ID || ', produkt: ' || v_result(i).Produkt_ID ||
+            ', zapełnienie: ' || ROUND(v_result(i).Produkt_zapelnienie, 2) || '%'
+        );
+    END LOOP;
+END;
+/
