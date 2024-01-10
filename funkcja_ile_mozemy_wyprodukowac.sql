1a40dc98680d3b18ef91d5819d2055cdde1d1a36
PatrykW7 <116021501+PatrykW7@users.noreply.github.com>
2024-01-10 21:27:10 +0100

    Update funkcja_ile_mozemy_wyprodukowac.sql

--- funkcja_ile_mozemy_wyprodukowac.sql
+++ funkcja_ile_mozemy_wyprodukowac.sql
@@ -1,4 +1,78 @@
+/*
+  FUNKCJA KTORA WYSWIETLA ILE MOZEMY WYPRODUKOWAC DANYCH ASORTYMENTOW Z CZESCI W MAGAZYNIE
+*/
+
 -- DEKLARACJA FUNKCJI
+
+CREATE OR REPLACE FUNCTION WyswietlIloscProdukcji RETURN VARCHAR2 AS
+  TYPE ProduktInfo IS RECORD (
+    Produkt_ID Produkty.Produkt_ID%TYPE,
+    Nazwa_produktu Produkty.Nazwa_produktu%TYPE
+  );
+
+  TYPE CzescInfo IS RECORD (
+    Czesc_ID Produkt_czesci.Czesc_ID%TYPE,
+    Nazwa_czesci Czesci.Nazwa_czesci%TYPE,
+    Minimalne_dzielenie NUMBER
+  );
+
+  TYPE ProduktCzesciInfo IS TABLE OF CzescInfo INDEX BY BINARY_INTEGER;
+  TYPE WszystkieProduktyInfo IS TABLE OF ProduktInfo INDEX BY BINARY_INTEGER;
+
+  v_output VARCHAR2(32000);
+  v_produkty WszystkieProduktyInfo;
+BEGIN
+  -- Zbieranie informacji o produktach
+  SELECT DISTINCT p.Produkt_ID, p.Nazwa_produktu
+  BULK COLLECT INTO v_produkty
+  FROM Produkty p;
+
+  -- Przetwarzanie informacji o produktach i częściach
+  FOR i IN 1 .. v_produkty.COUNT LOOP
+    v_output := v_output || 'Produkt: ' || v_produkty(i).Nazwa_produktu || ', Produkt ID: ' || v_produkty(i).Produkt_ID || CHR(10);
+
+    DECLARE
+      v_czesci ProduktCzesciInfo;
+    BEGIN
+      SELECT pc.Czesc_ID, c.Nazwa_czesci, FLOOR(MIN(m.Ilosc_w_magazynie / pc.wymagana_ilosc_czesci)) AS minimalne_dzielenie
+      BULK COLLECT INTO v_czesci
+      FROM Produkt_czesci pc
+      JOIN Magazyn_czesci m ON pc.Czesc_ID = m.Czesc_ID
+      JOIN Czesci c ON m.Czesc_ID = c.Czesc_ID
+      WHERE pc.Produkt_ID = v_produkty(i).Produkt_ID
+      GROUP BY pc.Czesc_ID, c.Nazwa_czesci;
+
+      FOR j IN 1 .. v_czesci.COUNT LOOP
+        v_output := v_output || 'Część: ' || v_czesci(j).Nazwa_czesci || ', Część ID: ' || v_czesci(j).Czesc_ID || ', Liczba produktów do wyprodukowania z części: ' || v_czesci(j).minimalne_dzielenie || CHR(10);
+      END LOOP;
+    END;
+  END LOOP;
+
+  RETURN v_output;
+END;
+/
+
+
+-- WYWOLANIE FUNKCJI
+
+ DECLARE
+  v_wynik VARCHAR2(32000);
+BEGIN
+  v_wynik := WyswietlIloscProdukcji();
+  DBMS_OUTPUT.PUT_LINE(v_wynik);
+END;
+/
+ 
+
+
+
+
+
+
+
+
+
+
 
 CREATE OR REPLACE FUNCTION WyswietlIloscProdukcji RETURN VARCHAR2 AS
   v_output VARCHAR2(32000);
