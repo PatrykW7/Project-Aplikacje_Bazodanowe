d7b4be886adb485ede22ff842510a2a7308fcdcc
PatrykW7 <116021501+PatrykW7@users.noreply.github.com>
2024-01-10 23:50:02 +0100

    Update ONE_third_part.sql

--- ONE_third_part.sql
+++ ONE_third_part.sql
@@ -1,39 +1,12 @@
 /*
-LACZENIE FIRST_PART i SECOND_PART 
-napisz nową funkcje, ktora oblicza ile danej czesci brakuje, 
+
+Zawiera funkcje pomocnicza
 jako parametr przyjmuje kursor zbudowany z Czesc_ID i Ilosc_potrzebna 
 i oblicza różnicę między tym, a ilością danej części na magazynie, 
-wykorzystując tabele Magazyn_czesci
+wykorzystując tabele Magazyn_czesci, wyswietla ile danej czesci potrzeba i ile brakuje.
+
 */
 
--- BEZ KURSORA
-CREATE OR REPLACE FUNCTION ObliczBrakujaceCzesci(
-    czesc_id_p INT,
-    ilosc_potrzebna_p INT
-) RETURN INT IS
-    ilosc_w_magazynie INT;
-    brakujaca_ilosc INT;
-BEGIN
-    SELECT Ilosc_w_magazynie INTO ilosc_w_magazynie
-    FROM Magazyn_czesci
-    WHERE Czesc_ID = czesc_id_p;
-
-    IF ilosc_w_magazynie >= ilosc_potrzebna_p THEN
-        brakujaca_ilosc := 0;
-    ELSE
-        brakujaca_ilosc := ilosc_potrzebna_p - ilosc_w_magazynie;
-    END IF;
-
-    RETURN brakujaca_ilosc;
-EXCEPTION
-    WHEN NO_DATA_FOUND THEN
-        RAISE_APPLICATION_ERROR(-20001, 'Nie znaleziono informacji o danej części w magazynie.');
-    WHEN OTHERS THEN
-        RAISE_APPLICATION_ERROR(-20002, 'Wystąpił inny błąd.');
-END;
-/
-
--- Z KURSOREM
 
 CREATE OR REPLACE FUNCTION ObliczBrakujaceCzesci(
     czesc_id_p INT,
@@ -62,33 +35,7 @@ END;
 
 
 
-
--- WYWOLANIE 
-
-DECLARE
-    ilosc_czesci_cur SYS_REFCURSOR;
-    czesc_id Produkt_czesci.Czesc_ID%TYPE;
-    ilosc_potrzebna Produkt_czesci.wymagana_ilosc_czesci%TYPE;
-    brakujaca_ilosc INT;
-BEGIN
-    ilosc_czesci_cur := IloscPotrzebnychCzesci(2, 5); -- Przykład: ID produktu = 2, ilość sztuk = 5
-
-    LOOP
-        FETCH ilosc_czesci_cur INTO czesc_id, ilosc_potrzebna;
-        EXIT WHEN ilosc_czesci_cur%NOTFOUND;
-
-        -- Wywołanie funkcji ObliczBrakujaceCzesci dla każdej części z wyniku IloscPotrzebnychCzesci
-        brakujaca_ilosc := ObliczBrakujaceCzesci(czesc_id, ilosc_potrzebna);
-
-        DBMS_OUTPUT.PUT_LINE('Czesc_ID: ' || czesc_id || ', Ilosc potrzebna: ' || ilosc_potrzebna ||
-                             ', Brakujaca ilosc: ' || brakujaca_ilosc);
-    END LOOP;
-
-    CLOSE ilosc_czesci_cur;
-END;
-/
-
--- WYWOLANIE Z KURSOREM
+-- WYWOLANIE
 
 DECLARE
     ilosc_czesci_cur SYS_REFCURSOR;
@@ -120,3 +67,75 @@ BEGIN
     CLOSE ilosc_czesci_cur;
 END;
 /
+
+
+
+
+
+
+
+
+
+--------- TESTY
+    
+
+-- BEZ KURSORA
+CREATE OR REPLACE FUNCTION ObliczBrakujaceCzesci(
+    czesc_id_p INT,
+    ilosc_potrzebna_p INT
+) RETURN INT IS
+    ilosc_w_magazynie INT;
+    brakujaca_ilosc INT;
+BEGIN
+    SELECT Ilosc_w_magazynie INTO ilosc_w_magazynie
+    FROM Magazyn_czesci
+    WHERE Czesc_ID = czesc_id_p;
+
+    IF ilosc_w_magazynie >= ilosc_potrzebna_p THEN
+        brakujaca_ilosc := 0;
+    ELSE
+        brakujaca_ilosc := ilosc_potrzebna_p - ilosc_w_magazynie;
+    END IF;
+
+    RETURN brakujaca_ilosc;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Nie znaleziono informacji o danej części w magazynie.');
+    WHEN OTHERS THEN
+        RAISE_APPLICATION_ERROR(-20002, 'Wystąpił inny błąd.');
+END;
+/
+
+
+
+
+
+
+
+-- WYWOLANIE 
+
+DECLARE
+    ilosc_czesci_cur SYS_REFCURSOR;
+    czesc_id Produkt_czesci.Czesc_ID%TYPE;
+    ilosc_potrzebna Produkt_czesci.wymagana_ilosc_czesci%TYPE;
+    brakujaca_ilosc INT;
+BEGIN
+    ilosc_czesci_cur := IloscPotrzebnychCzesci(2, 5); -- Przykład: ID produktu = 2, ilość sztuk = 5
+
+    LOOP
+        FETCH ilosc_czesci_cur INTO czesc_id, ilosc_potrzebna;
+        EXIT WHEN ilosc_czesci_cur%NOTFOUND;
+
+        -- Wywołanie funkcji ObliczBrakujaceCzesci dla każdej części z wyniku IloscPotrzebnychCzesci
+        brakujaca_ilosc := ObliczBrakujaceCzesci(czesc_id, ilosc_potrzebna);
+
+        DBMS_OUTPUT.PUT_LINE('Czesc_ID: ' || czesc_id || ', Ilosc potrzebna: ' || ilosc_potrzebna ||
+                             ', Brakujaca ilosc: ' || brakujaca_ilosc);
+    END LOOP;
+
+    CLOSE ilosc_czesci_cur;
+END;
+/
+
+-- WYWOLANIE Z KURSOREM
+
