64f559e2c6bb0b17351aeea5f423cb872a96f423
PatrykW7 <116021501+PatrykW7@users.noreply.github.com>
2024-01-10 21:12:43 +0100

    Update procedura_przewidywany_czas_realizacji_zamowienia.sql

--- procedura_przewidywany_czas_realizacji_zamowienia.sql
+++ procedura_przewidywany_czas_realizacji_zamowienia.sql
@@ -24,22 +24,40 @@ INSERT ALL
     INTO Harmonogram_urlopow(Urlop_ID,PESEL,Od_kiedy,Do_kiedy) VALUES (2, '23258741369','21-MAR-05', '30-MAR-05')
 SELECT 1 FROM dual;
 
-
-TUTAJ PRZYDALOBY SIE ZROBIC ZEBY ILOSC NIE MOGLA BYC WIEKSZA OD ILOSCI W MAGAZYNIE, ALE JEBAC TO NARAZIE 
-
-
 */
 
+CREATE OR REPLACE TYPE SzczegolyZamowienia_Object AS OBJECT (
+    Szczegoly_zamowienia_ID INT,
+    Znizka FLOAT,
+    Lokacja_ID INT,
+    Zamowienie_ID INT,
+    Kwota FLOAT,
+    Rodzaj_platnosci VARCHAR(45),
+    Przewidywany_czas_realizacji DATE
+);
+/
+
+
 CREATE OR REPLACE PROCEDURE DodajSzczegolyZamowienia (
-    p_Szczegoly_zamowienia_ID IN Szczegoly_zamowienia.Szczegoly_zamowienia_ID%TYPE,
-    p_Znizka IN Szczegoly_zamowienia.Znizka%TYPE,
-    p_Lokacja_ID IN Szczegoly_zamowienia.Lokacja_ID%TYPE,
-    p_Zamowienie_ID IN Szczegoly_zamowienia.Zamowienie_ID%TYPE,
-    p_Kwota IN Szczegoly_zamowienia.Kwota%TYPE,
-    p_Rodzaj_platnosci IN Szczegoly_zamowienia.Rodzaj_platnosci%TYPE
+    p_Szczegoly_zamowienia SzczegolyZamowienia_Object
 ) AS
     v_Przewidywany_czas_realizacji NUMBER;
+	BAD_LOC EXCEPTION;
+	BAD_KWOTA EXCEPTION;
 BEGIN
+    SELECT COUNT(*)
+    INTO v_Przewidywany_czas_realizacji
+    FROM Magazyn_produktow
+    WHERE Lokacja_ID = p_Szczegoly_zamowienia.Lokacja_ID;
+
+    IF v_Przewidywany_czas_realizacji = 0 THEN
+        RAISE BAD_LOC;
+    END IF;
+
+	IF p_Szczegoly_zamowienia.Kwota <= 0 THEN
+        RAISE BAD_KWOTA;
+    END IF; 
+    
     SELECT COUNT(*)
     INTO v_Przewidywany_czas_realizacji
     FROM Harmonogram_urlopow hu
@@ -50,217 +68,52 @@ BEGIN
 
     IF v_Przewidywany_czas_realizacji = 0 THEN
         INSERT INTO Szczegoly_zamowienia (Szczegoly_zamowienia_ID, Znizka, Lokacja_ID, Zamowienie_ID, Kwota, Rodzaj_platnosci, Przewidywany_czas_realizacji)
-        VALUES (p_Szczegoly_zamowienia_ID, p_Znizka, p_Lokacja_ID, p_Zamowienie_ID, p_Kwota, p_Rodzaj_platnosci, SYSDATE);
+        VALUES (p_Szczegoly_zamowienia.Szczegoly_zamowienia_ID, p_Szczegoly_zamowienia.Znizka, p_Szczegoly_zamowienia.Lokacja_ID, p_Szczegoly_zamowienia.Zamowienie_ID, p_Szczegoly_zamowienia.Kwota, p_Szczegoly_zamowienia.Rodzaj_platnosci, SYSDATE);
     ELSE
         INSERT INTO Szczegoly_zamowienia (Szczegoly_zamowienia_ID, Znizka, Lokacja_ID, Zamowienie_ID, Kwota, Rodzaj_platnosci, Przewidywany_czas_realizacji)
-        VALUES (p_Szczegoly_zamowienia_ID, p_Znizka, p_Lokacja_ID, p_Zamowienie_ID, p_Kwota, p_Rodzaj_platnosci, SYSDATE + 3);
+        VALUES (p_Szczegoly_zamowienia.Szczegoly_zamowienia_ID, p_Szczegoly_zamowienia.Znizka, p_Szczegoly_zamowienia.Lokacja_ID, p_Szczegoly_zamowienia.Zamowienie_ID, p_Szczegoly_zamowienia.Kwota, p_Szczegoly_zamowienia.Rodzaj_platnosci, SYSDATE + 3);
     END IF;
 
     COMMIT;
     DBMS_OUTPUT.PUT_LINE('Dodano nowy rekord do tabeli Szczegoly_zamowienia.');
+
 EXCEPTION
     WHEN NO_DATA_FOUND THEN
         DBMS_OUTPUT.PUT_LINE('Brak danych w tabeli Harmonogram_urlopow.');
-
     WHEN VALUE_ERROR THEN
         DBMS_OUTPUT.PUT_LINE('Błąd: Nieprawidłowe typy danych.');
+	WHEN BAD_LOC THEN
+        DBMS_OUTPUT.PUT_LINE('Podane Lokacja_ID nie istnieje w tabeli Magazyn_produktow.');
+	WHEN BAD_KWOTA THEN
+        DBMS_OUTPUT.PUT_LINE('Błędna kwota. Kwota musi być większa od zera.');
     WHEN OTHERS THEN
         DBMS_OUTPUT.PUT_LINE('Wystąpił błąd: ' || SQLERRM);
         ROLLBACK;
 END;
 /
 
--- WYWOLANIE PROCEDURY
+
+
+
+
+-- WYWOLANIE
 
 DECLARE
-    v_Szczegoly_zamowienia_ID Szczegoly_zamowienia.Szczegoly_zamowienia_ID%TYPE := 17; 
-    v_Znizka Szczegoly_zamowienia.Znizka%TYPE := 0.1; 
-    v_Lokacja_ID Szczegoly_zamowienia.Lokacja_ID%TYPE := 1;
-    v_Zamowienie_ID Szczegoly_zamowienia.Zamowienie_ID%TYPE := 1; 
-    v_Kwota Szczegoly_zamowienia.Kwota%TYPE := 1000; 
-    v_Rodzaj_platnosci Szczegoly_zamowienia.Rodzaj_platnosci%TYPE := 'Gotowka'; 
+    szczegoly_zamowienia SzczegolyZamowienia_Object;
 BEGIN
-    DodajSzczegolyZamowienia(
-        p_Szczegoly_zamowienia_ID => v_Szczegoly_zamowienia_ID,
-        p_Znizka => v_Znizka,
-        p_Lokacja_ID => v_Lokacja_ID,
-        p_Zamowienie_ID => v_Zamowienie_ID,
-        p_Kwota => v_Kwota,
-        p_Rodzaj_platnosci => v_Rodzaj_platnosci
+    -- Inicjalizacja obiektu z danymi
+    szczegoly_zamowienia := SzczegolyZamowienia_Object(
+        Szczegoly_zamowienia_ID => 22,
+        Znizka => 0.1,
+        Lokacja_ID => 123,
+        Zamowienie_ID => 20,
+        Kwota => 100.0,
+        Rodzaj_platnosci => 'Gotówka',
+        Przewidywany_czas_realizacji => SYSDATE
     );
+
+    -- Wywołanie procedury z obiektem jako parametrem
+    DodajSzczegolyZamowienia(p_Szczegoly_zamowienia => szczegoly_zamowienia);
 END;
 /
 
-
--------------------
-
--- WERSJA KTORA NIE MA SYS + 3, tylko do_kiedy + 2 dni, ale CHUJ WIE CZY TO DZIALA NIE SPRAWDZALEM
-
-
-CREATE OR REPLACE PROCEDURE DodajSzczegolyZamowienia (
-    p_Szczegoly_zamowienia_ID IN Szczegoly_zamowienia.Szczegoly_zamowienia_ID%TYPE,
-    p_Znizka IN Szczegoly_zamowienia.Znizka%TYPE,
-    p_Lokacja_ID IN Szczegoly_zamowienia.Lokacja_ID%TYPE,
-    p_Zamowienie_ID IN Szczegoly_zamowienia.Zamowienie_ID%TYPE,
-    p_Kwota IN Szczegoly_zamowienia.Kwota%TYPE,
-    p_Rodzaj_platnosci IN Szczegoly_zamowienia.Rodzaj_platnosci%TYPE
-) AS
-    v_Przewidywany_czas_realizacji NUMBER;
-    v_Do_kiedy DATE;
-BEGIN
-    SELECT COUNT(*), MAX(hu.Do_kiedy)
-    INTO v_Przewidywany_czas_realizacji, v_Do_kiedy
-    FROM Harmonogram_urlopow hu
-    JOIN Pracownicy p ON hu.PESEL = p.PESEL
-    JOIN Zatrudnienie z ON p.id_zatrudnienia = z.id_zatrudnienia
-    WHERE z.Nazwa_stanowiska = 'Pracownik magazynu'
-      AND SYSDATE BETWEEN hu.Od_kiedy AND hu.Do_kiedy;
-
-    IF v_Przewidywany_czas_realizacji = 0 THEN
-        INSERT INTO Szczegoly_zamowienia (Szczegoly_zamowienia_ID, Znizka, Lokacja_ID, Zamowienie_ID, Kwota, Rodzaj_platnosci, Przewidywany_czas_realizacji)
-        VALUES (p_Szczegoly_zamowienia_ID, p_Znizka, p_Lokacja_ID, p_Zamowienie_ID, p_Kwota, p_Rodzaj_platnosci, SYSDATE);
-    ELSE
-        INSERT INTO Szczegoly_zamowienia (Szczegoly_zamowienia_ID, Znizka, Lokacja_ID, Zamowienie_ID, Kwota, Rodzaj_platnosci, Przewidywany_czas_realizacji)
-        VALUES (p_Szczegoly_zamowienia_ID, p_Znizka, p_Lokacja_ID, p_Zamowienie_ID, p_Kwota, p_Rodzaj_platnosci, v_Do_kiedy + 2);
-    END IF;
-
-    COMMIT;
-    DBMS_OUTPUT.PUT_LINE('Dodano nowy rekord do tabeli Szczegoly_zamowienia.');
-EXCEPTION
-    WHEN NO_DATA_FOUND THEN
-        DBMS_OUTPUT.PUT_LINE('Brak danych w tabeli Harmonogram_urlopow.');
-    WHEN VALUE_ERROR THEN
-        DBMS_OUTPUT.PUT_LINE('Błąd: Nieprawidłowe typy danych.');
-    WHEN OTHERS THEN
-        DBMS_OUTPUT.PUT_LINE('Wystąpił błąd: ' || SQLERRM);
-        ROLLBACK;
-END;
-/
-
-
-
--- TUTAJ WERSJA CO SPRAWDZA CZY Nazwa_asortymentu jest w bazie danych, tez chuj wie czy to dziala
-
-CREATE OR REPLACE PROCEDURE DodajSzczegolyZamowienia (
-    p_Szczegoly_zamowienia_ID IN Szczegoly_zamowienia.Szczegoly_zamowienia_ID%TYPE,
-    p_Znizka IN Szczegoly_zamowienia.Znizka%TYPE,
-    p_Lokacja_ID IN Szczegoly_zamowienia.Lokacja_ID%TYPE,
-    p_Zamowienie_ID IN Szczegoly_zamowienia.Zamowienie_ID%TYPE,
-    p_Kwota IN Szczegoly_zamowienia.Kwota%TYPE,
-    p_Rodzaj_platnosci IN Szczegoly_zamowienia.Rodzaj_platnosci%TYPE
-) AS
-    v_Przewidywany_czas_realizacji NUMBER;
-    v_CountProdukt NUMBER;
-BEGIN
-    -- Sprawdzenie czy podany asortyment istnieje w tabeli Produkty
-    SELECT COUNT(*)
-    INTO v_CountProdukt
-    FROM Produkty
-    WHERE Nazwa_produktu = 'nazwa_asortymentu'; -- Tutaj wpisz nazwę asortymentu
-
-    IF v_CountProdukt = 0 THEN
-        DBMS_OUTPUT.PUT_LINE('Nieprawidłowa nazwa asortymentu.');
-    ELSE
-        SELECT COUNT(*)
-        INTO v_Przewidywany_czas_realizacji
-        FROM Harmonogram_urlopow hu
-        JOIN Pracownicy p ON hu.PESEL = p.PESEL
-        JOIN Zatrudnienie z ON p.id_zatrudnienia = z.id_zatrudnienia
-        WHERE z.Nazwa_stanowiska = 'Pracownik magazynu'
-          AND SYSDATE BETWEEN hu.Od_kiedy AND hu.Do_kiedy;
-
-        IF v_Przewidywany_czas_realizacji = 0 THEN
-            INSERT INTO Szczegoly_zamowienia (Szczegoly_zamowienia_ID, Znizka, Lokacja_ID, Zamowienie_ID, Kwota, Rodzaj_platnosci, Przewidywany_czas_realizacji)
-            VALUES (p_Szczegoly_zamowienia_ID, p_Znizka, p_Lokacja_ID, p_Zamowienie_ID, p_Kwota, p_Rodzaj_platnosci, SYSDATE);
-        ELSE
-            INSERT INTO Szczegoly_zamowienia (Szczegoly_zamowienia_ID, Znizka, Lokacja_ID, Zamowienie_ID, Kwota, Rodzaj_platnosci, Przewidywany_czas_realizacji)
-            VALUES (p_Szczegoly_zamowienia_ID, p_Znizka, p_Lokacja_ID, p_Zamowienie_ID, p_Kwota, p_Rodzaj_platnosci, SYSDATE + 3);
-        END IF;
-    END IF;
-
-    COMMIT;
-    --DBMS_OUTPUT.PUT_LINE('Dodano nowy rekord do tabeli Szczegoly_zamowienia.');
-
-EXCEPTION
-    WHEN NO_DATA_FOUND THEN
-        DBMS_OUTPUT.PUT_LINE('Brak danych w tabeli Harmonogram_urlopow.');
-    WHEN VALUE_ERROR THEN
-        DBMS_OUTPUT.PUT_LINE('Błąd: Nieprawidłowe typy danych.');
-    WHEN OTHERS THEN
-        DBMS_OUTPUT.PUT_LINE('Wystąpił błąd: ' || SQLERRM);
-        ROLLBACK;
-END;
-/
-
--- WYWOLANIE 
-
-
-BEGIN
-    DodajSzczegolyZamowienia(
-        p_Szczegoly_zamowienia_ID => 123, -- Twój ID
-        p_Znizka => 0.1, -- Przykładowa zniżka
-        p_Lokacja_ID => 456, -- ID lokacji
-        p_Zamowienie_ID => 789, -- ID zamówienia
-        p_Kwota => 100.50, -- Przykładowa kwota
-        p_Rodzaj_platnosci => 'Gotówka' -- Przykładowy rodzaj płatności
-    );
-END;
-/
-
-
-
-------- ZMODYFIKOWANA WERSJA WPROWADZANIE DANYCH ALE NIE WYDAJE MI SIE ZEBY TO DZIALALO
-
-CREATE OR REPLACE PROCEDURE DodajSzczegolyZamowienia (
-    p_Szczegoly_zamowienia_ID IN Szczegoly_zamowienia.Szczegoly_zamowienia_ID%TYPE,
-    p_Znizka IN Szczegoly_zamowienia.Znizka%TYPE,
-    p_Lokacja_ID IN Szczegoly_zamowienia.Lokacja_ID%TYPE,
-    p_Zamowienie_ID IN Szczegoly_zamowienia.Zamowienie_ID%TYPE,
-    p_Kwota IN Szczegoly_zamowienia.Kwota%TYPE,
-    p_Rodzaj_platnosci IN Szczegoly_zamowienia.Rodzaj_platnosci%TYPE
-) AS
-    v_Przewidywany_czas_realizacji NUMBER;
-    v_Ilosc_w_magazynie INT;
-BEGIN
-    -- Sprawdzenie ilości w magazynie dla danej Lokacja_ID
-    SELECT Ilosc_w_magazynie INTO v_Ilosc_w_magazynie
-    FROM Magazyn_produktow
-    WHERE Lokacja_ID = p_Lokacja_ID; -- Tutaj sprawdzana jest ilość dla konkretnego Lokacja_ID
-
-    IF v_Ilosc_w_magazynie >= p_Ilosc THEN -- Warunek ilości w magazynie
-        SELECT COUNT(*)
-        INTO v_Przewidywany_czas_realizacji
-        FROM Harmonogram_urlopow hu
-        JOIN Pracownicy p ON hu.PESEL = p.PESEL
-        JOIN Zatrudnienie z ON p.id_zatrudnienia = z.id_zatrudnienia
-        WHERE z.Nazwa_stanowiska = 'Pracownik magazynu'
-          AND SYSDATE BETWEEN hu.Od_kiedy AND hu.Do_kiedy;
-
-        IF v_Przewidywany_czas_realizacji = 0 THEN
-            INSERT INTO Szczegoly_zamowienia (Szczegoly_zamowienia_ID, Znizka, Lokacja_ID, Zamowienie_ID, Kwota, Rodzaj_platnosci, Przewidywany_czas_realizacji)
-            VALUES (p_Szczegoly_zamowienia_ID, p_Znizka, p_Lokacja_ID, p_Zamowienie_ID, p_Kwota, p_Rodzaj_platnosci, SYSDATE);
-        ELSE
-            INSERT INTO Szczegoly_zamowienia (Szczegoly_zamowienia_ID, Znizka, Lokacja_ID, Zamowienie_ID, Kwota, Rodzaj_platnosci, Przewidywany_czas_realizacji)
-            VALUES (p_Szczegoly_zamowienia_ID, p_Znizka, p_Lokacja_ID, p_Zamowienie_ID, p_Kwota, p_Rodzaj_platnosci, SYSDATE + 3);
-        END IF;
-
-        -- Dodanie danych do tabeli Zamowienia
-        INSERT INTO Zamowienia (Zamowienie_ID, Data_zamowienia)
-        VALUES (p_Zamowienie_ID, SYSDATE);
-
-        COMMIT;
-        DBMS_OUTPUT.PUT_LINE('Dodano nowy rekord do tabeli Szczegoly_zamowienia i Zamowienia.');
-    ELSE
-        DBMS_OUTPUT.PUT_LINE('Nie można dodać zamówienia. Brak wystarczającej ilości w magazynie.');
-    END IF;
-
-EXCEPTION
-    WHEN NO_DATA_FOUND THEN
-        DBMS_OUTPUT.PUT_LINE('Brak danych w tabeli Harmonogram_urlopow.');
-    WHEN VALUE_ERROR THEN
-        DBMS_OUTPUT.PUT_LINE('Błąd: Nieprawidłowe typy danych.');
-    WHEN OTHERS THEN
-        DBMS_OUTPUT.PUT_LINE('Wystąpił błąd: ' || SQLERRM);
-        ROLLBACK;
-END;
-/
