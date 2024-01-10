e18dd5d5ada13f7cd1a7d13d8b938a9b558805a7
PatrykW7 <116021501+PatrykW7@users.noreply.github.com>
2024-01-10 23:18:18 +0100

    Update 10_procedura_aktualizuj_magazyn_czesci_aktualizuj_magazyn_produktow.sql

--- 10_procedura_aktualizuj_magazyn_czesci_aktualizuj_magazyn_produktow.sql
+++ 10_procedura_aktualizuj_magazyn_czesci_aktualizuj_magazyn_produktow.sql
@@ -1,64 +1,13 @@
 
 /*
 Sprawdza ile czesci potrzeba na dana ilosc asortymentu i sprawdza czy jest tyle czesci w magazynie, a jesli tak to
-usuwa dana ilosc czesci 
+usuwa dana ilosc czesci i aktualizuje wartosc ilosc_w_magazynie dla Magazyn_produktow dla danego Produkt_ID
 
-TO SAMO CO 9 +
-+ dodaje dane do tabeli Magazyn_produktow <- W CHUJ TO NIE DZIALA ALE SYF 
+
+
 */
 
 
-
-CREATE OR REPLACE PROCEDURE AktualizujMagazynCzesci(p_czesci_cur SYS_REFCURSOR) AS
-    v_czesc_id Magazyn_czesci.Czesc_ID%TYPE;
-    v_ilosc_potrzebna INT;
-    v_brakuje BOOLEAN := false;
-BEGIN
-    LOOP
-        FETCH p_czesci_cur INTO v_czesc_id, v_ilosc_potrzebna;
-        EXIT WHEN p_czesci_cur%NOTFOUND;
-
-        -- Sprawdzenie czy ilość potrzebna jest większa od ilości w magazynie
-        FOR magazyn IN (SELECT Ilosc_w_magazynie FROM Magazyn_czesci WHERE Czesc_ID = v_czesc_id) LOOP
-            IF v_ilosc_potrzebna > magazyn.Ilosc_w_magazynie THEN
-                v_brakuje := true;
-                EXIT;
-            END IF;
-        END LOOP;
-
-        IF v_brakuje THEN
-            DBMS_OUTPUT.PUT_LINE('Brak dla Czesc_ID ' || v_czesc_id || ' - ' || v_ilosc_potrzebna);
-            v_brakuje := false;
-        ELSE
-            -- Aktualizacja ilości w magazynie
-            UPDATE Magazyn_czesci
-            SET Ilosc_w_magazynie = Ilosc_w_magazynie - v_ilosc_potrzebna
-            WHERE Czesc_ID = v_czesc_id;
-
-            DBMS_OUTPUT.PUT_LINE('Zaktualizowano Magazyn_czesci dla Czesc_ID ' || v_czesc_id);
-            
-            -- Aktualizacja ilości w magazynie produktów
-            UPDATE Magazyn_produktow
-            SET Ilosc_w_magazynie = Ilosc_w_magazynie + v_ilosc_potrzebna
-            WHERE Produkt_ID = (SELECT Produkt_ID FROM Produkt_czesci WHERE Czesc_ID = v_czesc_id);
-
-            DBMS_OUTPUT.PUT_LINE('Zaktualizowano Magazyn_produktow dla Produkt_ID ' || v_czesc_id);
-        END IF;
-    END LOOP;
-
-    CLOSE p_czesci_cur;
-EXCEPTION
-    WHEN NO_DATA_FOUND THEN
-        DBMS_OUTPUT.PUT_LINE('Nie znaleziono informacji o danej części w magazynie.');
-    WHEN OTHERS THEN
-        DBMS_OUTPUT.PUT_LINE('Wystąpił inny błąd.');
-END;
-/
-
-
-
-
-------------------------- TO NIE DZIALA, NIE USUWA Z CZESCI :/ 
 
 
 CREATE OR REPLACE PROCEDURE AktualizujIloscWMagazynie (
@@ -140,3 +89,56 @@ EXCEPTION
     WHEN OTHERS THEN
         DBMS_OUTPUT.PUT_LINE('Wystąpił błąd: ' || SQLERRM);
 END;
+
+
+
+-- ORYGINAL
+
+CREATE OR REPLACE PROCEDURE AktualizujMagazynCzesci(p_czesci_cur SYS_REFCURSOR) AS
+    v_czesc_id Magazyn_czesci.Czesc_ID%TYPE;
+    v_ilosc_potrzebna INT;
+    v_brakuje BOOLEAN := false;
+BEGIN
+    LOOP
+        FETCH p_czesci_cur INTO v_czesc_id, v_ilosc_potrzebna;
+        EXIT WHEN p_czesci_cur%NOTFOUND;
+
+        -- Sprawdzenie czy ilość potrzebna jest większa od ilości w magazynie
+        FOR magazyn IN (SELECT Ilosc_w_magazynie FROM Magazyn_czesci WHERE Czesc_ID = v_czesc_id) LOOP
+            IF v_ilosc_potrzebna > magazyn.Ilosc_w_magazynie THEN
+                v_brakuje := true;
+                EXIT;
+            END IF;
+        END LOOP;
+
+        IF v_brakuje THEN
+            DBMS_OUTPUT.PUT_LINE('Brak dla Czesc_ID ' || v_czesc_id || ' - ' || v_ilosc_potrzebna);
+            v_brakuje := false;
+        ELSE
+            -- Aktualizacja ilości w magazynie
+            UPDATE Magazyn_czesci
+            SET Ilosc_w_magazynie = Ilosc_w_magazynie - v_ilosc_potrzebna
+            WHERE Czesc_ID = v_czesc_id;
+
+            DBMS_OUTPUT.PUT_LINE('Zaktualizowano Magazyn_czesci dla Czesc_ID ' || v_czesc_id);
+            
+            -- Aktualizacja ilości w magazynie produktów
+            UPDATE Magazyn_produktow
+            SET Ilosc_w_magazynie = Ilosc_w_magazynie + v_ilosc_potrzebna
+            WHERE Produkt_ID = (SELECT Produkt_ID FROM Produkt_czesci WHERE Czesc_ID = v_czesc_id);
+
+            DBMS_OUTPUT.PUT_LINE('Zaktualizowano Magazyn_produktow dla Produkt_ID ' || v_czesc_id);
+        END IF;
+    END LOOP;
+
+    CLOSE p_czesci_cur;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        DBMS_OUTPUT.PUT_LINE('Nie znaleziono informacji o danej części w magazynie.');
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Wystąpił inny błąd.');
+END;
+/
+
+
+
